7c1a7264835a7065119be424339d4a2a039df9c5
239developer <factoriogmer@gmail.com>
2022-01-29 18:45:19 +0300

    Loading Screen

---
+++ FROG/Assets/UI/Scripts/LoadingScreen.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadingScreen : MonoBehaviour
+{
+    public static int sceneID = 1;
+    public Image img;
+    private AsyncOperation loadingOperation;
+
+    void Start()
+    {
+        loadingOperation = SceneManager.LoadSceneAsync(sceneID);
+    }
+
+    void Update()
+    {
+        img.fillAmount = Mathf.Clamp01(loadingOperation.progress / 0.9f);
+        if(loadingOperation.progress >= 0.9f)
+        {
+            loadingOperation.allowSceneActivation = true;
+        }
+    }
+}
