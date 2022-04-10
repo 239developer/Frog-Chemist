bcb5ad298174d0314ad98045de40ba2ddd64370c
239developer <factoriogmer@gmail.com>
2022-04-10 18:52:55 +0300

    now working w/ colors

--- FROG/Assets/PTable/Scripts/PTableCellScript.cs
+++ FROG/Assets/PTable/Scripts/PTableCellScript.cs
@@ -12,4 +12,48 @@ public class PTableCellScript : MonoBehaviour
         numText.text = num.ToString();
         nameText.text = name;
     }
+
+    public void SetColor(int index)
+    {
+        Color color;
+
+        switch(index)
+        {
+            case 0:
+                color = new Color(0.8f, 0.4f, 0.1f, 1.0f);
+                break;
+            case 1:
+                color = new Color(0.7f, 0.7f, 0.0f, 1.0f);
+                break;
+            case 2:
+                color = new Color(0.4f, 0.3f, 0.05f, 1.0f);
+                break;
+            case 3:
+                color = new Color(1.0f, 0.7f, 0.75f, 1.0f);
+                break;
+            case 4:
+                color = new Color(0.85f, 0.0f, 0.0f, 1.0f);
+                break;
+            case 5:
+                color = new Color(0.1f, 0.1f, 0.9f, 1.0f);
+                break;
+            case 6:
+                color = new Color(0.1f, 0.8f, 0.8f, 1.0f);
+                break;
+            case 7:
+                color = new Color(0.4f, 1f, 0.1f, 1.0f);
+                break;
+            case 8:
+                color = new Color(0.9f, 0.1f, 0.9f, 1.0f);
+                break;
+            case 9:
+                color = new Color(0.67f, 0.67f, 0.67f, 1.0f);
+                break;
+            default:
+                color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+                break;
+        }
+
+        GetComponent<Image>().color = color;
+    }
 }
--- FROG/Assets/PTable/Scripts/TableManager.cs
+++ FROG/Assets/PTable/Scripts/TableManager.cs
@@ -4,21 +4,20 @@ using UnityEngine;
 
 public class TableManager : MonoBehaviour
 {
-    public GameObject defaultCell;
+    public GameObject parentObject, defaultCell;
     public float cellSpacing = 170.0f;
     public GameObject[] cells = {};
 
-    private GameObject canvas;
-
     void CreateCell(int x, int y) //empty cell
     {
-        GameObject cell = GameObject.Instantiate(defaultCell, new Vector3(x * cellSpacing, -y * cellSpacing, 0.0f), Quaternion.identity, canvas.transform);
+        GameObject cell = GameObject.Instantiate(defaultCell, new Vector3(x * cellSpacing, -y * cellSpacing, 0.0f), Quaternion.identity, parentObject.transform);
     }
 
-    void CreateCell(int x, int y, int num, string name)
+    void CreateCell(int x, int y, int num, string name, int colorIndex)
     {
-        GameObject cell = GameObject.Instantiate(defaultCell, new Vector3(x * cellSpacing, -y * cellSpacing, 0.0f), Quaternion.identity, canvas.transform);
+        GameObject cell = GameObject.Instantiate(defaultCell, new Vector3(x * cellSpacing, -y * cellSpacing, 0.0f), Quaternion.identity, parentObject.transform);
         cell.GetComponent<PTableCellScript>().SetText(num, name);
+        cell.GetComponent<PTableCellScript>().SetColor(colorIndex);
         cell.name = name;
     }
 
@@ -40,7 +39,7 @@ public class TableManager : MonoBehaviour
                         break;
 
                     default: //positives
-                        CreateCell(x, y, n, TableInfo.elems[n - 1]);
+                        CreateCell(x, y, n, TableInfo.elems[n - 1], TableInfo.group[n - 1]);
                         break;
                 }
             }
@@ -49,7 +48,6 @@ public class TableManager : MonoBehaviour
 
     void Start()
     {
-        canvas = GameObject.Find("Canvas");
         GenerateTable(TableInfo.pattern);
     }
 }
@@ -66,5 +64,10 @@ public struct TableInfo
     //-1 == delete cell; 0 == nothing; other numbers are for atomic numbers
     public static int[] pattern = {1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 2, 3, 4, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 5, 6, 7, 8, 9, 10,  11, 12, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 13, 14, 15, 16, 17, 18,   19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36,    37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54,     55, 56, 0, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86,      87, 88, 0, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118,       -1, -1, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, -1, -1, -1, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, -1};
     
+    //0 - alkali metals; 1 - alkaline earth metals; 2 - Lanthanoids; 3 - Actinoids;
+    //4 - transition metals; 5 - Post-transition metals; 6 - Metalloids;
+    //7 - Reactive nonmetals; 8 - Noble gases; 9 - unknown
+    public static int[] group = {7, 8, 0, 1, 6, 7, 7, 7, 7, 8, 0, 1, 5, 6, 7, 7, 7, 8, 0, 1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 6, 6, 7, 7, 8, 0, 1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 6, 6, 7, 8, 0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 6, 8, 0, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9};
+
     // public static int weights = ;
 }
