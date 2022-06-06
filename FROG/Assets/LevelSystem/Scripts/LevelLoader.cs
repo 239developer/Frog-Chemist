f2af9ffcf9ea89c91dc10f617be3379b46d01d88
239developer <factoriogmer@gmail.com>
2022-06-06 21:07:56 +0300

    working on level system

---
+++ FROG/Assets/LevelSystem/Scripts/LevelLoader.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class LevelLoader : MonoBehaviour
+{
+    // loads the level whose number is indicated in the button ->
+    public void LoadLevelByButton(GameObject button)
+    {
+        int id = Convert.ToInt32(button.GetComponentInChildren<Text>().text);
+        
+        Debug.Log(id);
+    }   
+}
